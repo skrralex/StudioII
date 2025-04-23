a7440f9701ea585ef6db39a2371e38ec2f608f4e
skrralex <148615303+skrralex@users.noreply.github.com>
2025-04-23 19:51:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const square = document.getElementById('square');
+function enableRotation(square) {
+  let center = { x: 0, y: 0 };
+  let initialAngle = 0;
+  let currentRotation = 0;
 
-let center = { x: 0, y: 0 };
-let initialAngle = 0;
-let currentRotation = 0;
+  square.addEventListener('touchstart', (e) => {
+    const rect = square.getBoundingClientRect();
+    center = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
 
-square.addEventListener('touchstart', (e) => {
-  const rect = square.getBoundingClientRect();
-  center = {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2
-  };
+    const touch = e.touches[0];
+    initialAngle = Math.atan2(touch.clientY - center.y, touch.clientX - center.x);
+  });
 
-  const touch = e.touches[0];
-  initialAngle = Math.atan2(touch.clientY - center.y, touch.clientX - center.x);
-});
+  square.addEventListener('touchmove', (e) => {
+    const touch = e.touches[0];
+    const newAngle = Math.atan2(touch.clientY - center.y, touch.clientX - center.x);
+    const angleDiff = newAngle - initialAngle;
 
-square.addEventListener('touchmove', (e) => {
-  const touch = e.touches[0];
-  const newAngle = Math.atan2(touch.clientY - center.y, touch.clientX - center.x);
-  const angleDiff = newAngle - initialAngle;
+    currentRotation += angleDiff * (180 / Math.PI);
+    square.style.transform = `translate(-50%, -50%) rotate(${currentRotation}deg)`;
 
-  // Convert radians to degrees and add to total rotation
-  currentRotation += angleDiff * (180 / Math.PI);
+    initialAngle = newAngle;
+  });
+}
 
-  square.style.transform = `rotate(${currentRotation}deg)`;
-
-  // Update initialAngle for smooth continuous rotation
-  initialAngle = newAngle;
-});
+enableRotation(document.getElementById('square-small'));
+enableRotation(document.getElementById('square-big'));

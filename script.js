9703ff97618368d7e47e26c35b96282b9784fb52
skrralex <148615303+skrralex@users.noreply.github.com>
2025-04-23 19:33:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const square = document.getElementById('square');
 
 let lastTouchX = 0;
-let rotation = 0;
+let currentRotation = 0;
 
 square.addEventListener('touchstart', (e) => {
   lastTouchX = e.touches[0].clientX;
 });
 
 square.addEventListener('touchmove', (e) => {
-  const currentX = e.touches[0].clientX;
-  const deltaX = currentX - lastTouchX;
-  lastTouchX = currentX;
+  const touchX = e.touches[0].clientX;
+  const deltaX = touchX - lastTouchX;
+  lastTouchX = touchX;
 
-  rotation += deltaX; // drag right to increase, left to decrease
-  square.style.transform = `rotate(${rotation}deg)`;
+  // Add the movement delta to the current rotation
+  currentRotation += deltaX;
+  
+  // Apply rotation
+  square.style.transform = `rotate(${currentRotation}deg)`;
 });
